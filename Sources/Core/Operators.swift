2f5b9bd9812268b7181a8694f375f0043b549727
kibigo! <marrus-sh@users.noreply.github.com>
2021-03-07 21:56:07 -0800

    Add Uncertain values as double‐optionals (for now)

--- Sources/Core/Operators.swift
+++ Sources/Core/Operators.swift
@@ -4,11 +4,12 @@
 //
 //  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-/// Codepoint·equal.
+
+/// Deep·nil·coalescing.
 ///
 ///  +  Version:
 ///     `0.2.0`.
-infix operator •=•: ComparisonPrecedence
+infix operator ???: NilCoalescingPrecedence
 
 /// Exclusion.
 ///
@@ -27,6 +28,24 @@ infix operator ÷=: AssignmentPrecedence
 ///  +  Version:
 ///     `0.2.0`.
 infix operator ×: TimesPrecedence
+
+/// Codepoint·equal.
+///
+///  +  Version:
+///     `0.2.0`.
+infix operator •=•: ComparisonPrecedence
+
+/// As·optional.
+///
+///  +  Version:
+///     `0.2.0`.
+postfix operator .?
+
+/// As·force·unwrapped·optional.
+///
+///  +  Version:
+///     `0.2.0`.
+postfix operator .!
 
 /// Zero·or·one·of.
 ///
